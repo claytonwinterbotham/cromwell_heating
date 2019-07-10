850a6e262826d9af4b7d0ae48d8aafceffb98a23
Clayton <cwinterbotham@gmail.com>
2019-07-10 15:47:21 -0700

    change site title

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -35,7 +35,7 @@ class LandingPage extends React.Component {
         <Header
           color="transparent"
           routes={dashboardRoutes}
-          brand="Material Kit React"
+          brand="Cromwell Heating"
           rightLinks={<HeaderLinks />}
           fixed
           changeColorOnScroll={{
